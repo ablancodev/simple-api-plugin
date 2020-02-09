be53c2315011b2458905f2a6a1852e1d5d137d2b
blanco <blanco@localhost>
2020-02-09 08:07:02 +0100

    Basics get and post calls

--- core/class-simple-api-plugin.php
+++ core/class-simple-api-plugin.php
@@ -4,5 +4,48 @@ class Simple_API_Plugin {
 	public static function init() {
 		
 	}
+	
+	// GET
+	public static function get( $url ) {
+	    
+	    // With authentication
+	    /*
+	    $args = array(
+	        'headers' => array(
+	            'Authorization' => 'Basic ' . base64_encode( YOUR_USERNAME . ':' . YOUR_PASSWORD )
+	        )
+	    );
+	    wp_remote_get( $url, $args );
+	    */
+	    
+	    $response = wp_remote_get( $url );
+	    if( is_wp_error( $response ) ) {
+	        return false;
+	    }
+	    $http_code = wp_remote_retrieve_response_code( $response );
+	    if ( $http_code != '200' ) {
+	        return false;
+	    }
+	    
+	    $body = wp_remote_retrieve_body( $response );
+	    
+	    return $body;
+	}
+	
+	/**
+	 * 
+	 * @param string $url
+	 * @param array $data
+	 */
+	public static function post( $url, $data ) {
+	    $response = wp_remote_post( $url, array(
+	        'body'    => $data,
+	        'headers' => array(
+	            'Authorization' => 'Basic ' . base64_encode( $username . ':' . $password ),
+	        ),
+	    ) );
+	    return $response;
+	}
+	
 }
 Simple_API_Plugin::init();
